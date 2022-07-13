777ea3889a6f7fe119833bfd120467795727c28b
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2022-07-13 15:32:08 +0300

    add login formik

--- src/features/autorization/Login.tsx
+++ src/features/autorization/Login.tsx
@@ -1,11 +1,75 @@
 import React from 'react';
+import {useFormik} from "formik";
+import style from "./Login.module.scss"
 
 const Login = () => {
-    return (
-        <div>
-           Login
-        </div>
-    );
-};
+
+
+    const formik = useFormik({
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z\d._%+-]+@[A-Z\d.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (values.password.length < 4) {
+                errors.password = 'password is short';
+            }
+            return errors;
+        },
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values))
+            formik.resetForm()
+        }
+    })
+    /*
+        if (isLoggedIn) {
+            return <Redirect to={"/"} />
+        }*/
+
+
+    return <div>
+
+        <form className={style.form} onSubmit={formik.handleSubmit}>
+            <>
+                <input
+                    type={"email"}
+                    placeholder="Email"
+                    {...formik.getFieldProps("email")}
+                />
+                {formik.touched.email && formik.errors.email ?
+                    <div style={{color: "red"}}>{formik.errors.email}</div> : null}
+                <input
+                    type="password"
+                    placeholder="Password"
+                    {...formik.getFieldProps("password")}
+                />
+                {formik.touched.password && formik.errors.password ?
+                    <div style={{color: "red"}}>{formik.errors.password}</div> : null}
+                <label> Remember me </label>
+                <input
+                    type={"checkbox"}
+                    {...formik.getFieldProps("rememberMe")}
+                    checked={formik.values.rememberMe}
+                />
+
+                <button type={'submit'} className={style.button}>Login</button>
+
+            </>
+        </form>
+    </div>
+}
 
 export default Login;
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
